bc2a3229b1dc5f3ff3dddb9591e168d42357bfb0
Guillaume Rager <william.rager@gmail.com>
2023-03-20 14:20:00 +0000

    generate list of icons
    + new icons

---
+++ generate_list_of_icons.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+folder_path = "./icons" # replace with the path to the folder you want to list
+
+os.remove(folder_path+"/list.json")
+
+folder_contents = os.listdir(folder_path)
+folder_contents.sort()
+# listdir() method returns a list of all files and folders in the given folder_path
+
+json_output = json.dumps({"allicons":folder_contents})
+# dumps() method converts the folder_contents list to a JSON-formatted string
+
+print(json_output)
+# prints the JSON output to the console
+
+with open(folder_path+"/list.json", "w") as f:
+    f.write(json_output)
